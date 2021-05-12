8ac6e7a0ee57833a537c7b3eab5ca25a15d4d8bb
Ingrid Nogueira <isnogueira601@gmail.com>
2021-05-12 17:11:40 -0300

    update

--- App01.java
+++ App01.java
@@ -10,8 +10,8 @@ public class App01 {
         private static int anoCriacao;
         private static double prova1;
         private static double prova2;       
-        private static double trab1;
-        private static double trab2;       
+        private static double trabalho1;
+        private static double trabalho2;       
 
         public static boolean validar(int qtd){
             return qtd == 10;
@@ -24,93 +24,57 @@ public class App01 {
             nomeEscola = parametros[3];
             emailEscola = parametros[4];
             anoCriacao = Integer.valueOf(parametros[5]);
-            prova1 = Double.valueOf(parametros[6]);
-            prova2 = Double.valueOf(parametros[7]);
-            trab1 = Double.valueOf(parametros[8]);
-            trab2 = Double.valueOf(parametros[9]);
+            prova1 = validarProva(Double.valueOf(parametros[6]));
+            prova2 = validarProva(Double.valueOf(parametros[7]));
+            trabalho1 = validarTrabalho(Double.valueOf(parametros[8]));
+            trabalho2 = validarTrabalho(Double.valueOf(parametros[9]));
         }
 
         private static String getTipo(){
-            return bolsista == true ? "bolsista" : "pagante";
+            return bolsista ? "bolsista" : "pagante";
         }
 
         private static float getMensalidade(){
-            if(bolsista == true){
-                return mensalidade /= 2;
-            }
-            else{
-                return mensalidade;
-            }
+            return bolsista ? mensalidade/2 : mensalidade; 
         }
 
-        private static double validarProva1(){
-            if (prova1 < 0){
+        private static double validarProva(double prv){
+            if (prv < 0){
               return 0;
             }
-            else if(prova1 > 7 ){
+            else if(prv > 7 ){
                 return 7;
             }
             else{
-                return prova1;
+                return prv;
             }
         }
-        
-        private static double validarProva2(){
-             if (prova2 < 0){
-                 return 0;
-            }
-             else if(prova2 > 7){
-                 return 7;
-             }
-             else {
-                return prova2;
-             }
-        }
-                         
-        private static double validarTrab1(){
-             if (trab1 < 0){
+          
+        private static double validarTrabalho(double trab){
+             if (trab < 0){
                  return 0;
              }
-             else if(trab1 > 3){
+             else if(trab > 3){
                  return 3;
              }
              else{
-                 return trab1;
+                 return trab;
              }
         }
-                
-        private static double validarTrab2(){
-             if (trab2 < 0){
-                 return 0;
-             }
-             else if(trab1 > 3){
-                 return 3;
-             }
-             else{
-                 return trab2;
-             }
-        }
-        
+
          private static double calcularMedia(){
-            double prova1 = validarProva1();
-            double prova2 = validarProva2();
-            double trab1 = validarTrab1();
-            double trab2 = validarTrab2();
-            
-            double nota1 = (prova1 + trab1) * 0.6;
-            double nota2 = (prova2 + trab2) * 0.4;
-            return nota1 + nota2;
+            return ((prova1 + trabalho1) * 0.6) + ((prova2 + trabalho2) * 0.4);
 
         }
 
-        private static String getSituacao(){
-          double media = calcularMedia();
+        private static String getSituacao(double media){
           return media >= 8 ? "aprovado" : "reprovado";
 
         }
 
         private static void exibir() {
-            System.out.println( nomeAluno + " - aluno(a) " + getTipo() + " - paga R$" + getMensalidade() + " de mensalidade em " + nomeEscola + "(" + emailEscola + ")" + " - fundado(a) em " + anoCriacao + " - e foi " + getSituacao() + " com media "  + calcularMedia() + ".");
+            double media = calcularMedia();
+            System.out.printf(nomeAluno + " - aluno(a) " + getTipo() + " - paga R$" + getMensalidade() + " de mensalidade em " + nomeEscola + "(" + emailEscola + ")" + " - fundado(a) em " + anoCriacao + " - e foi " + getSituacao(media) + " com media " +  media + ".");
 
         }
 
